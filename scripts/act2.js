afeecd8eaede4775dba2d0582e73859ae7b43222
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2023-09-24 18:05:07 -0400

    changed colors in act 2

--- scripts/act2.js
+++ scripts/act2.js
@@ -296,7 +296,7 @@ function init() {
   });
   effect.setSize(window.innerWidth * 0.9, window.innerHeight * 1.075);
   effect.domElement.style.color = "black";
-  effect.domElement.style.backgroundColor = "white";
+  effect.domElement.style.backgroundColor = "LightGray";
   effect.domElement.style.overflow = "hidden";
   document.body.appendChild(effect.domElement);
 
